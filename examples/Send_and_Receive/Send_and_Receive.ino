2e6ea02d9e80a924677ecee30993b366a01b3a65
saidm5797 <43006210+saidm5797@users.noreply.github.com>
2020-04-26 17:09:16 -0500

    Update Send_and_Receive.ino

--- examples/Send_and_Receive/Send_and_Receive.ino
+++ examples/Send_and_Receive/Send_and_Receive.ino
@@ -1,7 +1,6 @@
-// CAN Send and Receive Example
-//
-
+// CAN Send and Receive Simple Example
 #include <mcp_can.h>
+#include <TTU_IsoTp.h>
 #include <SPI.h>
 #include <TTUCAN.h>
 
@@ -10,17 +9,9 @@ unsigned char len = 0;
 unsigned char rxBuf[8];
 char msgString[128];                        // Array to store serial string
 byte data[8] = {0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22};
-//example variables
-float temp = 17.5;
-int count = 325;
-double speed_ = 35.5;
-float length_ = 126.8;
 
 #define CAN0_INT 2                              // Set INT to pin 2
-TTUCAN CAN0(10, CAN0_INT, 3, 0);                               // Set CS to pin 10
-
-INT32U preProcess(byte *data, INT32U to_addr, INT32U descriptor); //create data array from variables
-void postProcess(byte *data, INT32U receiveID); //update variables from data array
+TTUCAN CAN0(10, CAN0_INT, 4, 1);      // Set CS to pin 10
 
 void setup()
 {
@@ -31,7 +22,6 @@ void setup()
 
 void loop()
 {
-  
   if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
   {
     CAN0.receive_Msg(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
@@ -56,70 +46,10 @@ void loop()
     Serial.println();
   }
 
-  txId = preProcess(data, 2, 3); //(data array,to_address, descriptor)
+
+  txId = CAN0.buildTransmitID(5, 5); //(to_address, descriptor)
+  Serial.println(txId, HEX);
   CAN0.send_Msg(txId, 0, data, sizeof(data)); //(CAN ID, rtr, data array, len of data)
-  delay(random(100,250)); //wait random time between 100-250 milliseconds
-}
-
-INT32U preProcess(byte *data, INT32U to_addr, INT32U descriptor){
-  //each case is for a different ID 
-  //users should plan ahead and know the contents of each message
-  //in order to process information correctly
-  INT32U transmitID = CAN0.buildTransmitID(to_addr, descriptor);
-  int _size;
+  delay(250); //wait 250 milliseconds
   
-  switch(transmitID){
-    case 0:
-      _size = sizeof(temp);
-      memcpy(data, (byte *)&temp, sizeof(temp));
-      memcpy((data+_size), (byte *)&count, sizeof(count));
-      _size += sizeof(count);
-      memcpy((data+_size), (byte *)&speed_, sizeof(speed_));
-      _size += sizeof(speed_);
-      memcpy((data+_size), (byte *)&length_, sizeof(length_));
-      break;
-    default:
-      _size = sizeof(temp);
-      memcpy(data, (byte *)&temp, sizeof(temp));
-      memcpy((data+_size), (byte *)&count, sizeof(count));
-      _size += sizeof(count);
-      memcpy((data+_size), (byte *)&speed_, sizeof(speed_));
-      break;
-  }
-
-  return transmitID;
-}
-
-void postProcess(byte *data, INT32U receiveID){
-  //each case is for a different ID 
-  //users should plan ahead and know the contents of each message
-  //in order to process information correctly
-  int _size;
-  switch(receiveID){
-    case 0: 
-      _size = sizeof(temp); //starting index of data array
-      memcpy(&temp, data, sizeof(temp));
-      memcpy(&count, (data +_size), sizeof(count));
-      _size +=sizeof(count); //increment index of array by size of count to access the next variable
-      memcpy(&speed_, (data +_size), sizeof(speed_));
-      _size +=sizeof(speed_);   //increment index of array by size of speed_ to access the next variable
-      memcpy(&length_, (data +_size), sizeof(length_)); // store value from array into variable to be used in program
-      
-      Serial.println(temp);
-      Serial.println(count);
-      Serial.println(speed_);
-      Serial.println(length_);
-      break;
-    default:
-      _size = sizeof(temp);
-      memcpy(&temp, data, sizeof(temp));
-      memcpy(&count, (data +_size), sizeof(count));
-      _size +=sizeof(count);
-      memcpy(&speed_, (data +_size), sizeof(speed_));
-  
-      Serial.println(temp);
-      Serial.println(count);
-      Serial.println(speed_);
-      break;
-  }
 }
